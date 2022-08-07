20612faaf7085b83d9b05f931142242ae3518579
Siddharth <siddv.karne@gmail.com>
2022-08-08 01:15:51 +0530

    Create Tensors_tutorial1.py

---
+++ Tensors_tutorial1.py
@@ -0,0 +1,27 @@
+import torch
+import numpy as np #including relevant libraries
+
+#ways to init a tensor
+
+data = [[1, 2], [3, 4]]
+x_data = torch.tensor(data)
+
+np_array = np.array(data)
+x_np = torch.from_numpy(np_array)
+
+ones = torch.ones_like(x_data) #retains properties of x_data
+print(f"Ones Tensor: \n {ones} \n")
+
+rand = torch.rand_like(x_data, dtype=torch.float) #overrides/changes the datatype in x_data
+print(f"Random Tensor: \n {rand} \n")
+
+#shape refers to the dimensions of a tensor
+
+shape = (2,3, )
+rand_tensor = torch.rand(shape)
+ones_tensor = torch.ones(shape)
+zeros_tensor = torch.zeros(shape)
+
+print(f"Random Tensor: \n {rand_tensor} \n")
+print(f"Ones Tensor: \n {ones_tensor} \n")
+print(f"Zeros Tensor: \n {zeros_tensor} \n")
